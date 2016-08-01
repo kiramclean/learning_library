1986a8f58b35fe4117df44e59b0cec2ee8a19cb4
Kira McLean <kiraemclean@gmail.com>
2016-08-01 09:17:31 -0300

    root to resources index for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'resources#index'
   resources :resources
 
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
