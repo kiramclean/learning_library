fb479be89a01f2fe278f34e19872fc198b650da3
Kira McLean <kiraemclean@gmail.com>
2016-08-03 21:08:12 -0400

    always report code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
-if ENV.fetch('COVERAGE', false)
-  require 'simplecov'
-  require 'codecov'
+require 'simplecov'
+require 'codecov'
 
-  if ENV['CIRCLE_ARTIFACTS']
-    dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-    SimpleCov.coverage_dir(dir)
-  end
-
-  SimpleCov.start
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
 end
+
+SimpleCov.start { add_filter '/spec/' }
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 require 'spec_helper'
 require 'webmock/rspec'
