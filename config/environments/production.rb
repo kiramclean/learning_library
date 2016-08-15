2f2ecee2d1070a0f3a50644a5880ccaba883a4a3
Kira McLean <kiraemclean@gmail.com>
2016-08-14 23:36:08 -0400

    default config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,15 +4,15 @@ Rails.application.configure do
   # if ENV.fetch('HEROKU_APP_NAME', '').include?('staging-pr-')
   #   ENV['APPLICATION_HOST'] = ENV['HEROKU_APP_NAME'] + '.herokuapp.com'
   # end
-  config.middleware.use Rack::CanonicalHost, ENV.fetch('APPLICATION_HOST')
+  # config.middleware.use Rack::CanonicalHost, ENV.fetch('APPLICATION_HOST')
   config.cache_classes = true
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
-  config.action_controller.asset_host = ENV.fetch('ASSET_HOST', ENV.fetch('APPLICATION_HOST'))
+  # config.action_controller.asset_host = ENV.fetch('ASSET_HOST', ENV.fetch('APPLICATION_HOST'))
   config.log_level = :debug
   config.log_tags = [:request_id]
   config.action_mailer.perform_caching = false
@@ -31,4 +31,4 @@ Rails.application.configure do
   config.static_cache_control = 'public, max-age=31557600'
   config.action_mailer.default_url_options = { host: ENV.fetch('APPLICATION_HOST') }
 end
-Rack::Timeout.timeout = (ENV['RACK_TIMEOUT'] || 10).to_i
+# Rack::Timeout.timeout = (ENV['RACK_TIMEOUT'] || 10).to_i
